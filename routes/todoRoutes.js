98dba8e44f3399cd50170345d984be764b3ec766
Roche <florianrga@gmail.com>
2024-11-14 08:56:26 +0100

    Project done (I could check data before pushing it)

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -6,7 +6,7 @@ const todoController = require("../controllers/todoController");
 // Définition des routes
 router.post("/create", todoController.createTodo); // Créer une tâche
 router.get("/todos", todoController.getTodos); // Lire toutes les tâches
-router.put("/edit/:id", todoController.updateTodo); // Mettre à jour une tâche
+router.put("/update/:id", todoController.updateTodo); // Mettre à jour une tâche
 router.delete("/delete/:id", todoController.deleteTodo); // Supprimer une tâche
 router.get("/:id", todoController.getATodo); // Supprimer une tâche
 
