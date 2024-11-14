94e36408f50b9acbaa3488c5e4c12098dfb40244
Roche <florianrga@gmail.com>
2024-11-14 07:03:32 +0100

    Front API

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ const todoRoutes = require("./routes/todoRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+	origin: "http://localhost:3000",
+	methods: ["GET", "POST", "PUT", "DELETE"],
+	allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Connexion à la base de données
 mongoose
